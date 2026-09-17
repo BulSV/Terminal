fa93ec28b66c133c01b70ae4ebe4c94f501f0e5e
BulSV <bulsv.global@gmail.com>
2026-09-18 00:01:33 +0300

    Refactor macro management and update connections

--- Macros.cpp
+++ Macros.cpp
@@ -7,6 +7,9 @@
 #include <QtMath>
 #include <QStatusBar>
 #include <QSet>
+#include <QDir>
+#include <QFileDialog>
+#include <QTimer>
 
 #include "Macros.h"
 
@@ -65,21 +68,20 @@ Macros::Macros(QWidget *parent)
 
     scrollAreaLayout->setSpacing(0);
     scrollAreaLayout->setContentsMargins(0, 0, 0, 0);
-    scrollAreaLayout->addSpacerItem(new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding));
+    scrollAreaLayout->addSpacerItem(new QSpacerItem(0, 0, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding));
 
     mainWidget->setLayout(scrollAreaLayout);
 
     QDir dir;
-    if(!dir.exists(dir.currentPath() + "/Macros")) {
-        dir.mkpath(dir.currentPath() + "/Macros");
+    if(!dir.exists(QDir::currentPath() + "/Macros")) {
+        dir.mkpath(QDir::currentPath() + "/Macros");
     }
-    fileDialog->setDirectory(dir.currentPath() + "/Macros");
+    fileDialog->setDirectory(QDir::currentPath() + "/Macros");
     fileDialog->setFileMode(QFileDialog::ExistingFiles);
     fileDialog->setNameFilter(tr("Terminal Macro File (*.tmf)"));
 
     connect(actionNew, &QAction::triggered, this, &Macros::addMacro);
-    connect(actionDelete, &QAction::triggered,
-            this, static_cast<void (Macros::*)()>(&Macros::deleteMacros));
+    connect(actionDelete, &QAction::triggered, this, qOverload<>(&Macros::deleteMacros));
     connect(actionLoad, &QAction::triggered, this, &Macros::loadMacros);
     connect(actionStartStop, &QAction::triggered, this, &Macros::startOrStop);
     connect(actionPause, &QAction::triggered, this, &Macros::pause);
@@ -158,7 +160,7 @@ void Macros::addMacro()
     macros.append(macro);
     scrollAreaLayout->insertWidget(scrollAreaLayout->count() - 1, macro);
 
-    connect(macro, &Macro::deleted, this, static_cast<void (Macros::*)()>(&Macros::deleteMacro));
+    connect(macro, &Macro::deleted, this, qOverload<>(&Macros::deleteMacro));
     connect(macro, &Macro::packetSended, this, &Macros::packetSended);
     connect(macro, &Macro::selected, this, &Macros::updateIntervals);
     connect(macro, &Macro::timeChanged, this, &Macros::calculateMultiSendCeiledTime);
@@ -166,7 +168,7 @@ void Macros::addMacro()
     connect(macro, &Macro::movedDown, this, &Macros::moveMacroDown);
     connect(actionSelectMacros, &QAction::triggered, macro, &Macro::select);
     connect(actionDeselectMacros, &QAction::triggered, macro, &Macro::deselect);
-    connect(spinBoxTime, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), this, &Macros::setSelectedMacrosTime);
+    connect(spinBoxTime, &QSpinBox::valueChanged, this, &Macros::setSelectedMacrosTime);
 }
 
 void Macros::deleteMacro()
@@ -182,7 +184,7 @@ void Macros::deleteMacro(Macro *macro)
     macro->deselect();
     macros.removeOne(macro);
     scrollAreaLayout->removeWidget(macro);
-    disconnect(macro, &Macro::deleted, this, static_cast<void (Macros::*)()>(&Macros::deleteMacro));
+    disconnect(macro, &Macro::deleted, this, qOverload<>(&Macros::deleteMacro));
     disconnect(macro, &Macro::packetSended, this, &Macros::packetSended);
     disconnect(macro, &Macro::selected, this, &Macros::updateIntervals);
     disconnect(macro, &Macro::timeChanged, this, &Macros::calculateMultiSendCeiledTime);
@@ -190,204 +192,53 @@ void Macros::deleteMacro(Macro *macro)
     disconnect(macro, &Macro::movedDown, this, &Macros::moveMacroDown);
     disconnect(actionSelectMacros, &QAction::triggered, macro, &Macro::select);
     disconnect(actionDeselectMacros, &QAction::triggered, macro, &Macro::deselect);
-    disconnect(spinBoxTime, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), this, &Macros::setSelectedMacrosTime);
+    disconnect(spinBoxTime, &QSpinBox::valueChanged, this, &Macros::setSelectedMacrosTime);
     delete macro;
     macro = 0;
 }
 
-void Macros::deleteMacros()
+void Macros::moveMacro(Macro *macro, MacrosMoveDirection direction)
 {
-    int button = QMessageBox::question(this, tr("Warning"),
-                                       tr("Delete macros?"),
-                                       QMessageBox::Yes | QMessageBox::No);
-    if(button == QMessageBox::Yes) {
-        QListIterator<Macro*> it(macros);
-        while(it.hasNext()) {
-            deleteMacro(it.next());
-        }
-        macros.clear();
-        indexesOfIntervals.clear();
-        currentIntervalIndex = 0;
-    }
-}
-
-void Macros::moveMacroUp()
-{
-    moveMacro(qobject_cast<Macro*>(sender()), MoveUp);
-}
-
-void Macros::moveMacroDown()
-{
-    moveMacro(qobject_cast<Macro*>(sender()), MoveDown);
-}
-
-void Macros::moveMacro(Macro *macro, Macros::MacrosMoveDirection direction)
-{
-    if(!macro) {
-        return;
-    }
-
     int macroIndex = macros.indexOf(macro);
-
-    QVBoxLayout* tempLayout = qobject_cast<QVBoxLayout*>(macro->parentWidget()->layout());
-    int index = tempLayout->indexOf(macro);
-
-    if(direction == MoveUp) {
-        if(macroIndex == 0) {
-            return;
-        }
-
-        macros.swap(macroIndex, macroIndex - 1);
-        --index;
-    } else {
-        if(macroIndex == macros.size() - 1) {
-            return;
-        }
-
-        macros.swap(macroIndex, macroIndex + 1);
-        ++index;
-    }
-    tempLayout->removeWidget(macro);
-    tempLayout->insertWidget(index, macro);
-    std::sort(indexesOfIntervals.begin(), indexesOfIntervals.end(), qLess<int>());
-}
-
-void Macros::loadMacros()
-{
-    QStringList fileNames;
-    if(fileDialog->exec()) {
-        fileNames = fileDialog->selectedFiles();
-    }
-
-    QListIterator<QString> it(fileNames);
-    while(it.hasNext()) {
-        addMacro();
-        macros.last()->openMacroFile(it.next());
-    }
-}
-
-void Macros::startOrStop()
-{
-    if(indexesOfIntervals.isEmpty()) {
-        return;
-    }
-    if(actionStartStop->toolTip() == tr("Start sending macros")) {
-        actionStartStop->setIcon(QIcon(":/Resources/Stop.png"));
-        actionStartStop->setToolTip(tr("Stop sending macros"));
-        actionPause->setEnabled(true);
-        blockForMultiSend(true);
-        sendNextMacro();
-    } else {
-        actionStartStop->setIcon(QIcon(":/Resources/Play.png"));
-        actionStartStop->setToolTip(tr("Start sending macros"));
-        actionPause->setChecked(false);
-        actionPause->setToolTip("Pause sending macros");
-        actionPause->setEnabled(false);
-        intervalTimer->stop();
-        currentIntervalIndex = 0;
-        blockForMultiSend(false);
-    }
-}
-
-void Macros::pause(bool check)
-{
-    if(check) {
-        intervalTimer->stop();
-        actionPause->setToolTip("Resume sending macros");
-    } else {
-        intervalTimer->start();
-        actionPause->setToolTip("Pause sending macros");
-    }
-}
-
-void Macros::updateIntervals(bool add)
-{
-    Macro *macro = qobject_cast<Macro*>(sender());
-    if(macro == 0) {
+    if(macroIndex == -1) {
         return;
     }
 
-    int index = macros.indexOf(macro);
-
-    if(add) {
-        indexesOfIntervals.append(index);
-        QSet<int> set = indexesOfIntervals.toSet();
-        indexesOfIntervals = set.toList();
-        std::sort(indexesOfIntervals.begin(), indexesOfIntervals.end(), qLess<int>());
-    } else {
-        indexesOfIntervals.removeOne(index);
+    if(direction == MacrosMoveDirection::Up && macroIndex > 0) {
+        macros.swapItemsAt(macroIndex, macroIndex - 1);
+        scrollAreaLayout->removeWidget(macro);
+        scrollAreaLayout->insertWidget(macroIndex - 1, macro);
+    } else if(direction == MacrosMoveDirection::Down && macroIndex < macros.count() - 1) {
+        macros.swapItemsAt(macroIndex, macroIndex + 1);
+        scrollAreaLayout->removeWidget(macro);
+        scrollAreaLayout->insertWidget(macroIndex + 1, macro);
     }
+    
+    std::sort(indexesOfIntervals.begin(), indexesOfIntervals.end(), std::less<int>());
+}
 
+void Macros::updateIntervals(bool)
+{
+    indexesOfIntervals.clear();
+    for(int i = 0; i < macros.count(); ++i) {
+        if(macros.at(i)->selectState()) {
+            indexesOfIntervals.append(i);
+        }
+    }
+    
+    QSet<int> set(indexesOfIntervals.begin(), indexesOfIntervals.end());
+    indexesOfIntervals = set.values(); 
+    
+    std::sort(indexesOfIntervals.begin(), indexesOfIntervals.end(), std::less<int>());
     calculateMultiSendCeiledTime();
 }
 
-void Macros::sendNextMacro()
+void Macros::deleteMacros()
 {
-    Macro *macro = macros.at(indexesOfIntervals.at(currentIntervalIndex++));
-    if(currentIntervalIndex >= indexesOfIntervals.size()) {
-        currentIntervalIndex = 0;
-        if(actionSendMode->toolTip() == DEFAULT_SEND_MODE) {
-
-            emit packetSended(macro->getPacket());
-
-            startOrStop();
-
-            return;
+    int button = QMessageBox::question(this, tr("Warning"), tr("Delete macros?"), QMessageBox::Yes | QMessageBox::No);
+    if (button == QMessageBox::Yes) {
+        while(!macros.isEmpty()) {
+            deleteMacro(macros.first());
         }
-    }
-
-    emit packetSended(macro->getPacket());
-
-    intervalTimer->start(macro->getTime());
-}
-
-void Macros::blockForMultiSend(bool block)
-{
-    actionDelete->setDisabled(block);
-    actionSelectMacros->setDisabled(block);
-    actionDeselectMacros->setDisabled(block);
-    spinBoxTime->setDisabled(block);
-    actionNew->setDisabled(block);
-    actionLoad->setDisabled(block);
-    actionSendMode->setDisabled(block);
-
-    QListIterator<Macro*> it(macros);
-    while(it.hasNext()) {
-        it.next()->setDisabled(block);
-    }
-}
-
-void Macros::calculateMultiSendCeiledTime()
-{
-    if(!packetTimeCalculator->isValid()) {
-        multiSentTime->setText(MULTI_SEND_TIME.arg("None"));
-
-        return;
-    }
-
-    QListIterator<int> it(indexesOfIntervals);
-    double totalTime = 0;
-    while(it.hasNext()) {
-        totalTime += macros.at(it.next())->getTime();
-    }
-    multiSentTime->setText(MULTI_SEND_TIME.arg(QString::number(totalTime, 'f', 3)));
-}
-
-void Macros::cycleSingleSendMode()
-{
-    if(actionSendMode->toolTip() == DEFAULT_SEND_MODE) {
-        actionSendMode->setIcon(QIcon(":/Resources/Cycle.png"));
-        actionSendMode->setToolTip(tr("Cycle sending mode"));
-    } else {
-        actionSendMode->setIcon(QIcon(":/Resources/SingleShot.png"));
-        actionSendMode->setToolTip(tr("Single shot sending mode"));
-    }
-}
-
-void Macros::setSelectedMacrosTime()
-{
-    QListIterator<int> it(indexesOfIntervals);
-    while(it.hasNext()) {
-        macros[it.next()]->setTime(spinBoxTime->value());
     }
 }
